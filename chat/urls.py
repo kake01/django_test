7ca01c4dd7bfe89f3c97942cde57e73adce52c2b
kake01 <kakerun1123@gmail.com>
2021-07-14 23:22:33 +0900

    formの一部バグ修正処理前

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 from .views import MessageView
 urlpatterns = [
-    path('', MessageView.as_view(), name='index'),
+    path('', MessageView.as_view(), name='chat_bot_message'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Message
+from .forms import  MessageForm
+from django.shortcuts import redirect
+
 
 class MessageView(TemplateView):
     def __init__(self):
@@ -11,8 +14,23 @@ class MessageView(TemplateView):
         data = Message.objects.all()
         self.params = {
             'data': data,
+            'form' : MessageForm(),
         }
         return render(request, 'chat/index.html', self.params)
 
     def post(self, request):
-        return render(request, 'chat/index.html',self.params)
+        #DBに保存
+        obj = Message()
+        # ユーザーDB作成したら外部キーに設定(TODO)
+        # obj.owner_id = request.user.id
+        # 返答は学習モデルを使用して作成(TODO)
+        # obj.respond = self.bot_respond(request.POST['content'])
+        obj.respond = 'self.bot_respond(request.POSTtent])'
+        form = MessageForm(request.POST, instance=obj)
+        form.save()
+        # return render(request, 'chat/index.html',self.params)
+        return redirect(to="/")
+    
+    # バックエンドメソッドを読み込み返答を作成(TODO)
+    def bot_respond(msg):
+        return '貴方は言いました。「' + msg + '」と。'
