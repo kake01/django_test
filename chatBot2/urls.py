df6adae054dd0b5a1c095866eeea60b9c45a980b
kake01 <kakerun1123@gmail.com>
2021-08-15 13:08:17 +0900

    音声ファイルの自動作成完了

--- chatBot2/urls.py
+++ chatBot2/urls.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 
-from django.conf.urls import url# //includeを追記
-from todo.urls import router as to_do_router#　//追記
-
 urlpatterns = [
     path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('home/', include('base.urls')),
     path('chat/', include('chat.urls')),
-    # path('todo/', include('todo.urls')),
-    # url(r'^api/', include(to_do_router.urls)), #//追記
-    url('todo/', include(to_do_router.urls)), #//追記
+    path('todo/', include('todo.urls')),
 ]
